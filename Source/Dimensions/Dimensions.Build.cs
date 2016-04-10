b3a5a8a7619f5ae4ea642c5514097e00123a117d
Cadrach <cadrach@gmail.com>
2016-04-10 22:35:19 +0200

    BLUI plugin integration

--- Source/Dimensions/Dimensions.Build.cs
+++ Source/Dimensions/Dimensions.Build.cs
@@ -8,7 +8,9 @@ public class Dimensions : ModuleRules
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.Add("Blu");
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
